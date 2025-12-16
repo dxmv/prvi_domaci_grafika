19a7e55736bc158160e2cdf773e74ed85903fccb
dxmv <dimitirjestepanovic1@gmail.com>
2025-12-16 22:41:09 +0100

    gradient effect

--- src/main_state.c
+++ src/main_state.c
@@ -94,18 +94,15 @@ void main_state_update(GLFWwindow *window, float delta_time, rafgl_game_data_t *
     //    dvostrukom for petljom prolazimo kroz svaki piksel rastera
     //    tacka (0, 0) je gornji levi ugao slike a tacka (w-1, h-1) je donji desni ugao
     for(y=0; y<h; y++){
-        for(x=0;x<w;x++){
-//            uz pomoc pixel_at_m makroa mozemo pristupiti pikselu na koordinatama x, y unutar rastera r (makro pixel_at_pm koristimo ako imamo pokazivac na raster)
-
-                /*
-                pixel je union i mozemo ga koristiti na vise nacina. ako imamo pixel pix i zelimo da mu promenimo jednu komponentu, na primer crvenu
-                mozemo da uradimo pix.r = 255; isto to mozemo uraditi kao pix.components[0] = 255; ako zelimo da promenimo sve komponente istovremeno
-                (zadamo neku specificnu boju, na primer zutu) mozemo da uradimo pix.rgba = rafgl_RGB(255, 255, 0);
-                */
-
-            /* u ovom primeru je zadato da se raster popuni flat bojom (ista boja na celom rasteru),
-            tako da jednostavno mozemo svaki piksel postaviti da ima istu vrednost */
-            pixel_at_m(raster, x, y).rgba = rafgl_RGB(0, 0, 0);
+        float normalized_y = (float)y / h;
+        for(x = 0; x < w; x++)
+        {
+            // Gradient from dark blue/purple (top) to black (bottom)
+            pixel_at_m(raster, x, y).rgba = rafgl_RGB(
+                0, 
+                (int)(20 * normalized_y), 
+                (int)(40 * normalized_y)
+            );
         }
     }
 
